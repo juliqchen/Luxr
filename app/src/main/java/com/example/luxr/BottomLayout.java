2d49edc0d78b8ee85f50882aa37cbff5b74b4be4
juliachen <jchen694@gatech.edu>
2017-06-30 12:06:08 -0700

    tab names

---
+++ app/src/main/java/com/example/luxr/BottomLayout.java
@@ -0,0 +1,14 @@
+package com.example.luxr;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by juliachen on 6/30/17.
+ */
+
+public class BottomLayout extends AppCompatActivity {
+
+
+
+
+}
