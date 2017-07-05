7ecd562362f5bf96405a3639fd733dde3151ac2c
jenniferhu <jennifer.hu@livecom>
2017-07-05 12:32:22 -0700

    doesn't work, set up style page

--- app/src/main/java/com/example/luxr/StyleActivity.java
+++ app/src/main/java/com/example/luxr/StyleActivity.java
@@ -16,7 +16,7 @@ public class StyleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.activity_style);
     }
 
     @Override
