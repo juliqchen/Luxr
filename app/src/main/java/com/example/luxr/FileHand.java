07e875ebe2ee24654f120233f5f9bd4f97947267
jenniferhu <jennifer.hu@livecom>
2017-07-05 14:40:08 -0700

    nothing works

--- app/src/main/java/com/example/luxr/FileHand.java
+++ app/src/main/java/com/example/luxr/FileHand.java
@@ -3,12 +3,9 @@ package com.example.luxr;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Environment;
-import android.support.v4.content.ContextCompat;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FilenameFilter;
-import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
