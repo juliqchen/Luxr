7e9a8ea6a8fdc450fdfe8695d070a6c6f2de6ca2
juliachen <jchen694@gatech.edu>
2017-07-11 14:31:25 -0700

    start making list view stuff

---
+++ app/src/main/java/com/example/luxr/ConfirmPhotoActivity.java
@@ -0,0 +1,22 @@
+package com.example.luxr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListView;
+
+/**
+ * Created by juliachen on 7/11/17.
+ */
+
+public class ConfirmPhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.photo_confirm);
+
+        ExpandableListView typeList = (ExpandableListView) findViewById(R.id.listType);
+
+
+    }
+}
