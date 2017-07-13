bc8dfccfba26fdcdfa3d7880ad75a21e7a54d660
jenniferhu <jennifer.hu@livecom>
2017-07-13 13:00:12 -0700

    expandable list view

--- app/src/main/java/com/example/luxr/ConfirmPhotoActivity.java
+++ app/src/main/java/com/example/luxr/ConfirmPhotoActivity.java
@@ -2,7 +2,13 @@ package com.example.luxr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by juliachen on 7/11/17.
@@ -10,13 +16,55 @@ import android.widget.ExpandableListView;
 
 public class ConfirmPhotoActivity extends AppCompatActivity {
 
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListDetail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.photo_confirm);
+        setContentView(R.layout.expandable_list_view_test);
 
-        ExpandableListView typeList = (ExpandableListView) findViewById(R.id.listType);
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+        expandableListDetail = ExpandableListViewData.getData();
+        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+        expandableListAdapter = new ExpandableListAdapter(this, expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
 
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Expanded.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Collapsed.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " -> " +
+                                expandableListDetail.get(expandableListTitle.get(groupPosition)).get(childPosition),
+                        Toast.LENGTH_SHORT
+                ).show();
+                return false;
+            }
+        });
+        //ExpandableListView typeList = (ExpandableListView) findViewById(R.id.listType);
     }
 }
+
