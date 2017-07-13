66282b69e4d7c16d0cef304a68442200a11f9a29
jenniferhu <jennifer.hu@livecom>
2017-07-13 14:00:12 -0700

    fixed pathing

--- app/src/main/java/com/example/luxr/StyleActivity.java
+++ app/src/main/java/com/example/luxr/StyleActivity.java
@@ -88,7 +88,7 @@ public class StyleActivity extends ListActivity {
 
     public void homeClicked() {
         View v = new View(this);
-        Intent intent = new Intent(v.getContext(), CameraActivity.class);
+        Intent intent = new Intent(v.getContext(), MainActivity.class);
         startActivity(intent);
     }
 
@@ -100,7 +100,7 @@ public class StyleActivity extends ListActivity {
 
     public void uploadClicked() {
         View v = new View(this);
-        Intent intent = new Intent(v.getContext(), MainActivity.class);
+        Intent intent = new Intent(v.getContext(), CameraActivity.class);
         startActivity(intent);
     }
 }
